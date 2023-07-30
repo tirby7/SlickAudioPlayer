7c02c6be0acea43f1db9bdb89245fe74b99586ca
tirby7 <135442774+tirby7@users.noreply.github.com>
2023-07-30 17:25:04 -0400

    Update Slick Audio Player.py

--- Slick Audio Player.py
+++ Slick Audio Player.py
@@ -21,6 +21,12 @@ import lyricsgenius
 
 
 class MusicPlayer:
+    ''' A simple music play application built using tkinter amd pygame.
+        Allow users to choose a music folder, display the list of availabe MP3 files,
+        and control music playback(play, pause, next, previous) along with volume control.
+        Additionally, the application displays animated GIFs while playing and pausing the music.
+        The music player also gives the user the ability to look up the lyrics of a file that is playing.'''
+    
     def __init__(self):
         # Dark and Light Mode
         customtkinter.set_appearance_mode("System")
@@ -165,8 +171,11 @@ class MusicPlayer:
     
     
     
-    #lets the user go into their files and click a music folder 
+   
     def open(self):
+        '''
+        Opens a file dialog to choose a music foldeer and displays the list of available MP3 files in the Listbox
+        '''
         global path
         global song
         path = filedialog.askdirectory()
@@ -178,14 +187,20 @@ class MusicPlayer:
                     self.musics.insert(END, song)
 
     def play_selected_song(self, event):
+        '''
+        Plays the selected song from the listbox
+        '''
         selected_song = self.musics.get(ACTIVE)
         mixer.music.load(selected_song)
         mixer.music.play()
     
     
     
-    #plays the music but also serves as a restart button when you press the play button  
+
     def Play_music(self):
+        '''
+        Plays the music but also serves as a restart button when you press the play button.
+        '''
         music_name = self.musics.get(ACTIVE)
         if len(music_name) > 40:
             self.song_name.config(text=music_name[0:40] + ".........")
@@ -202,8 +217,11 @@ class MusicPlayer:
     
     
     
-    #this is the function for the pause button where click the pause button will resume play from where it left off 
+   
     def Pause_Unpause(self):
+        ''' 
+        this is the function for the pause button where click the pause button will resume play from where it left off
+        '''
         music_name = self.musics.get(ACTIVE)
         if len(music_name) > 40:
             self.song_name.config(text=music_name[0:40] + ".........")
@@ -234,8 +252,13 @@ class MusicPlayer:
             self.Pause_button.config(textvariable="3")
     
     
-    #function for playing the next song 
+    
     def Next_song(self):
+        '''
+        
+        This function serves as the funciton for playing the next song when next button is pressed. 
+        
+        '''
         self.current_index += 1
         total_songs = self.musics.size()
 
@@ -265,8 +288,13 @@ class MusicPlayer:
     
     
     
-    #function for playing the previous song
+  
     def Prev_song(self):
+        '''
+        
+        This function is for playing the previous song when the back button is pressed.
+        
+        '''
         current_song = self.musics.curselection()
         select = current_song[0] - 1
         next_song = self.musics.get(select)
@@ -288,8 +316,12 @@ class MusicPlayer:
             self.animation_frame["text"] = "0"
     
     
-    #This is the code for the second tkinter window 
+  
     def display_lyrics_window(self):
+        '''
+        This is the code for displaying the second TkInter window.
+        '''
+        
         lyrics_window = Toplevel(self.window)
         lyrics_window.title("Lyrics")
 
@@ -308,9 +340,15 @@ class MusicPlayer:
         lyrics_text = Text(lyrics_window, wrap=WORD)
         lyrics_text.grid(row=3, columnspan=2, padx=5, pady=5)
 
-        #This is the code to get lyrics 
+       
         def get_lyrics(artist, song):
-            # This is the genius API where it pulls the lyrics from genius
+            '''
+           
+           This is the code for setting up the user to get the lyrics from the information inputted.  
+           
+           '''
+            
+            # This is the genius API where it pulls the lyrics from genius 
             genius = lyricsgenius.Genius("lJIC8GwL9tSEBTlypIOxHP59tvqZTiZlP9VMm68An0liV1tzRRXtXpDCYetf9H5K")
 
             try:
@@ -324,8 +362,12 @@ class MusicPlayer:
                 return "Error occurred while fetching lyrics."
 
         
-        #This is the code to display the lyrics
+        
         def display_lyrics():
+            '''
+            This code is for displaying the lyrics once the user presses the get lyrics button
+            '''
+            
             artist = artist_entry.get()
             song = song_entry.get()
             lyrics = get_lyrics(artist, song)
@@ -335,8 +377,11 @@ class MusicPlayer:
         get_lyrics_button = Button(lyrics_window, text="Get Lyrics", command=display_lyrics)
         get_lyrics_button.grid(row=2, columnspan=2, padx=5, pady=5)
    
-   #function for pausing the music and changing the animation
+   
     def Pause(self):
+        '''
+        Function for pausing the music and changing the animation. The gif will display a different motion when paused. 
+        '''
         global paused
         global change_anim
         self.change_anim = 0
@@ -344,8 +389,11 @@ class MusicPlayer:
         mixer.music.pause()
     
     
-    #function for controlling the volume of the music 
+    
     def volume(self, vol):
+        '''
+        This function is for controlling the volume of the music. 
+        '''
         Volume = int(vol) / 100
         if Volume >= 0.80:
             self.slider.config(fg='blue')
@@ -359,8 +407,12 @@ class MusicPlayer:
         mixer.music.set_volume(Volume)
     
     
-    #shows the playing time of the music 
+  
     def playing_time(self):
+        '''
+          This is for dispalying the playing time of the music. 
+        '''
+        
         current_position = mixer.music.get_pos() / 1000
 
         converted_time = time.strftime('%M:%S', time.gmtime(current_position))
@@ -383,8 +435,12 @@ class MusicPlayer:
     
     
     
-    #function for the listening animation when you press the play button 
+    
     def listening_animation(self, count):
+        '''
+        Function for the listening animation when you press the play button. 
+        The gif will play when you press the play button. 
+        '''
         frame_count = self.frame_list1[count]
         self.animation_frame.configure(image=frame_count)
         count += 1
@@ -395,8 +451,13 @@ class MusicPlayer:
     
     
     
-    #function for the animation when you press the pause button 
+    
     def not_listening_animation(self, count):
+        '''
+        Function for the animation when you press the pause button.
+        The gif will display the not_listening_animation when you press
+        the pause button to stop the music. 
+        '''
         frame_count = self.frame_list2[count]
         self.animation_frame.configure(image=frame_count)
         count += 1
